df9994d32b2ebbe6b03c2653c0c18f7791649858
yavorkoen <yavorkoen@gmail.com>
2022-07-07 22:25:56 +0300

    add intersectionObserver to sports

--- page-animations/sportIconsAnimation.js
+++ page-animations/sportIconsAnimation.js
@@ -1,7 +1,21 @@
 export function sportAnimation() {
     const icons = document.querySelector('.icon-container');
 
+    const observer = new IntersectionObserver(function (entries, observer) {
 
-    console.log(icons);
-    
+        entries.forEach(entry => {
+            console.log(entry.isIntersecting);
+            if (entry.isIntersecting) {
+                entry.target.style.animationName = 'slideIcons';
+                //    entry.target.style.animationIterationCount = 'infinite';
+            } else {
+                // entry.target.style.animationIterationCount = '0';
+                entry.target.style.animationName = null;
+
+            }
+        })
+    }, {});
+
+    observer.observe(icons)
+
 }
