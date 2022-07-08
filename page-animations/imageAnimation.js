21b3f5a33415243564e36f0703e3b399542dec19
yavorkoen <yavorkoen@gmail.com>
2022-07-08 12:47:08 +0300

    change travelling images animation

--- page-animations/imageAnimation.js
+++ page-animations/imageAnimation.js
@@ -1,15 +1,16 @@
 export function imageAnimation() {
     const images = Array.from(document.querySelectorAll('.image'));
     const otherInterests = document.querySelector('.other-interests');
+    const travelImages = document.querySelectorAll('.travel-image');
 
     const options = {
         // rootMargin: '-200px',
         threshold: thersholdGenerator()
     }
-    let observer = new IntersectionObserver(function(entries, observer){
+    let observer = new IntersectionObserver(function (entries, observer) {
         entries.forEach(entry => {
-            if(entry.isIntersecting) {
-                let rotateAngle = 15 - 15 *(entry.intersectionRatio * 5);
+            if (entry.isIntersecting) {
+                let rotateAngle = 15 - 15 * (entry.intersectionRatio * 5);
                 let position = 100 - entry.intersectionRatio * 200;
                 images[0].style.transform = `translateY(${position}px)`
                 images[1].style.transform = `rotateZ(${rotateAngle}deg)`;
@@ -17,11 +18,30 @@ export function imageAnimation() {
         })
     }, options);
     observer.observe(otherInterests);
+
+
+    let observerTravellingMedia = new IntersectionObserver(function (entries, observer) {
+        entries.forEach((entry) => {
+            console.log(entry.isIntersecting)
+            if (entry.isIntersecting) {
+                entry.target.style.transform = `scale(1)`;
+            } else {
+                entry.target.style.transform = 'scale(0.5)';
+            }
+        })
+    }, {
+        threshold: 0.50
+    });
+    travelImages.forEach((image, index) => {
+        image.style.transitionDelay = 100 * index + 'ms';
+        observerTravellingMedia.observe(image);
+    })
+
 }
 
 function thersholdGenerator() {
     let thresholds = [];
-    for(let i = 0; i <= 1; i+= 0.01){
+    for (let i = 0; i <= 1; i += 0.01) {
         thresholds.push(i.toFixed(2));
     }
     return thresholds;
