bce1c0d22d9781bf9c5d4b08634407e960be5cad
yavorkoen <yavorkoen@gmail.com>
2022-07-12 13:01:12 +0300

    add animation to  mobile nav links

--- app.js
+++ app.js
@@ -15,10 +15,26 @@ animateHeadings();
 imageAnimation();
 sportAnimation();
 
+function showNavMobile() {
 
-const menuBtn = document.querySelector('.menu-button');
-const navigationMobile = document.querySelector('.navigation-mobile');
-menuBtn.addEventListener('click', (e) => {
-    navigationMobile.classList.toggle("active");
-})
+    const menuBtn = document.querySelector('.menu-button');
+    const navigationMobile = document.querySelector('.navigation-mobile');
+    const navLinkMobile = document.querySelectorAll('.navigation-mobile li');
+    menuBtn.addEventListener('click', (e) => {
+        navigationMobile.classList.toggle('active');
+        if(navigationMobile.classList.contains('active')) {
+            menuBtn.textContent = 'x'
+            navLinkMobile.forEach((link, index )=> {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index/5}s`;
+            })
+        } else {
+            menuBtn.textContent = 'menu';
+            navLinkMobile.forEach((link, index )=> {
+                link.style.animation = ``; 
+            })
+        }
+    });
+}
+
+showNavMobile();
 
