808b2b0cd2026067ec92435d7cbecaf9baf4bd2d
yavorkoen <yavorkoen@gmail.com>
2022-07-28 13:56:17 +0300

    fix bug in sport section, change rules in media queries

--- page-animations/homeAnimation.js
+++ page-animations/homeAnimation.js
@@ -1,21 +1,21 @@
 export const homeAnimation = function () {
     const letters = document.querySelectorAll('.home span');
     const home = document.querySelector('.home');
-    const intersectionsOne = new Map();
+    const intersections = new Map();
     let options = {
         threshold: 0.6
     }
 
     let observer = new IntersectionObserver(function (entries, observer) {
         entries.forEach(entry => {
-            if (entry.isIntersecting && intersectionsOne.get(entry.target) != null) {
-                clearInterval(intersectionsOne.get(entry.target));
-                intersectionsOne.set(entry.target, setInterval(changeLetters, 200));
+            if (entry.isIntersecting && intersections.get(entry.target) != null) {
+                clearInterval(intersections.get(entry.target));
+                intersections.set(entry.target, setInterval(changeLetters, 300));
             } else if (entry.isIntersecting) {
-                intersectionsOne.set(entry.target, setInterval(changeLetters, 200));
-            } else if (!entry.isIntersecting && intersectionsOne.get(entry.target) != null) {
-                clearInterval(intersectionsOne.get(entry.target));
-                intersectionsOne.set(null);
+                intersections.set(entry.target, setInterval(changeLetters, 300));
+            } else if (!entry.isIntersecting && intersections.get(entry.target) != null) {
+                clearInterval(intersections.get(entry.target));
+                intersections.set(null);
                 letters.forEach(l => {
                     l.style.transform = 'scale(1)';
                 })
@@ -23,12 +23,12 @@ export const homeAnimation = function () {
             function changeLetters() {
                 for (let i = 0; i < Math.floor(Math.random() * 20); i++) {
                     if (i < 9) {
-                        letters[i].style.transform = 'scale(1.2)';
+                        letters[i].style.transform = 'scale(1)';
                     }
                 }
                 for (let i = 0; i < Math.floor(Math.random() * 20); i++) {
                     if (i < 9) {
-                        letters[i].style.transform = 'scale(0.5)';
+                        letters[i].style.transform = 'scale(0.7)';
                     }
                 }
             }
