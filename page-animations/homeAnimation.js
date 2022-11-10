4b7962dfcedfd72973f358b6b9657b69abaabfb1
Yavor Koen <yavorkoen@gmail.com>
2022-11-10 15:06:34 +0200

    home letter animation refactored

--- page-animations/homeAnimation.js
+++ page-animations/homeAnimation.js
@@ -1,29 +1,29 @@
 export const homeAnimation = function () {
     const letters = document.querySelectorAll('.home span');
     const home = document.querySelector('.home');
-    const intersections = new Map();
+    let intervalId = null;
     let options = {
-        threshold: 0.6
+        threshold: 0.7
     }
 
     let observer = new IntersectionObserver(function (entries, observer) {
         entries.forEach(entry => {
-            if (entry.isIntersecting && intersections.get(entry.target) != null) {
-                clearInterval(intersections.get(entry.target));
-                intersections.set(entry.target, setInterval(changeLetters, 200));
-            } else if (entry.isIntersecting) {
-                intersections.set(entry.target, setInterval(changeLetters, 200));
-            } else if (!entry.isIntersecting && intersections.get(entry.target) != null) {
-                clearInterval(intersections.get(entry.target));
-                intersections.set(null);
+            if(entry.isIntersecting) {
+                intervalId = setInterval(changeLetters, 200);
+            } else {
+                clearInterval(intervalId);
+                intervalId = null;
                 letters.forEach(l => {
-                    l.style.transform = 'scale(1)';
-                })
+                            l.style.transform = 'scale(1)';
+                        })
             }
             function changeLetters() {
                 for (let i = 0; i < Math.floor(Math.random() * 20); i++) {
                     if (i < 9) {
-                        letters[i].style.transform = 'scale(1)';
+                        letters[i].style.transform = 'scale(1.2)';
+                        if(i == 3 || i == 6) {
+                            letters[i].style.transform = 'scale(1.2) rotate(180deg)';
+                        }
                     }
                 }
                 for (let i = 0; i < Math.floor(Math.random() * 20); i++) {
