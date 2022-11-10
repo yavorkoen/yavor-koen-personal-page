91c5e90e37461d80e1acb2328e2ff601a9969c42
Yavor Koen <yavorkoen@gmail.com>
2022-11-10 12:47:23 +0200

    mobile links fadeIn/fadeOut animations added

--- page-animations/showHideNavMobile.js
+++ page-animations/showHideNavMobile.js
@@ -5,22 +5,25 @@ export function showHideNavMobile() {
     const navLinkMobile = document.querySelectorAll('.navigation-mobile li');
     menuBtn.addEventListener('click', (e) => {
         navigationMobile.classList.toggle('active');
-        if(navigationMobile.classList.contains('active')) {
-            menuBtn.textContent= 'X';
-            navLinkMobile.forEach((link, index )=> {
-                link.style.animation = `navLinkFade 0.5s ease forwards ${index/5}s`;
+        if (navigationMobile.classList.contains('active')) {
+            menuBtn.textContent = 'X';
+            navLinkMobile.forEach((link, index) => {
+                link.style.animation = `navLinkFadeIn 0.5s ease ${index / 7}s forwards`;
             })
         } else {
             menuBtn.textContent = 'menu';
-            navLinkMobile.forEach((link, index )=> {
-                link.style.animation = ``; 
+            navLinkMobile.forEach((link, index) => {
+                link.style.animation = `navLinkFadeOut ease ${index / 1.5}s forwards`;
             })
         }
     });
-        window.addEventListener('scroll', () => {
-            if(navigationMobile.classList.contains('active')) {
-                navigationMobile.classList.remove('active');
-                menuBtn.textContent = 'menu';
-            }
-        })
-    }
+    window.addEventListener('scroll', () => {
+        if (navigationMobile.classList.contains('active')) {
+            navigationMobile.classList.remove('active');
+            menuBtn.textContent = 'menu';
+            navLinkMobile.forEach((link, index) => {
+                link.style.animation = `navLinkFadeOut ease ${index / 5}s forwards`;
+            })
+        }
+    })
+}
